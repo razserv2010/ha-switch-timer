afd34419e3687e9cfc77727faa806b50748c99f5
Romans <romansa772@aol.com>
2024-12-05 15:05:18 +0200

    Fixed timer entity state bug, added entity validation for timer setting service. Version 1.0.1

--- custom_components/switch_timer/__init__.py
+++ custom_components/switch_timer/__init__.py
@@ -1,12 +1,8 @@
 import logging
 from homeassistant.helpers.event import track_time_interval, async_track_point_in_time
 from datetime import datetime, timedelta, timezone
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.config_entries import ConfigEntry
 import voluptuous as vol
-from .entity import TimerEntity
 
 
 _LOGGER = logging.getLogger(__package__)
@@ -17,9 +13,6 @@ unsub_dict = {}
 
 
 def setup(hass, config):
-
-    component = EntityComponent(_LOGGER, DOMAIN, hass)
-    hass.data[DOMAIN] = component
 
     def get_switch_entites():
         all_entities = hass.states.async_entity_ids()
@@ -39,9 +32,7 @@ def setup(hass, config):
             new_entity_name = DOMAIN + "." + entity_device_name
             
             if new_entity_name not in hass.states.async_entity_ids():
-                entity = TimerEntity(hass.states.get(entity).attributes.get("friendly_name", "Switch"))
-                entities.append(entity)
-        component.add_entities(entities)
+                hass.states.set(new_entity_name, "idle")
         
 
     add_timer_entities(get_switch_entites())
@@ -78,8 +69,15 @@ def setup(hass, config):
         return value
 
 
+    def validate_set_timer_entity(value):
+        if not value.startswith(DOMAIN+"."):
+            raise vol.Invalid("Invalid entity specified")
+        elif value not in hass.states.async_entity_ids():
+            raise vol.Invalid("Entity doesn't exsist")
+        return value
 
-    def validate_cancel_timer(value):
+
+    def validate_cancel_timer_entity(value):
         if value not in hass.states.async_entity_ids():
             raise vol.Invalid(f"Entity {value} doesn't exsist")
         elif value not in unsub_dict.keys():
@@ -88,22 +86,25 @@ def setup(hass, config):
         
 
     set_timer_schema = vol.Schema({
-        vol.Required('entity_id'): cv.string,
+        vol.Required('entity_id'): validate_set_timer_entity,
         vol.Required('action'): validate_action,
         vol.Required('duration'): validate_duration,
     })
 
     cancel_timer_schema = vol.Schema({
-        vol.Required('entity_id'): validate_cancel_timer
+        vol.Required('entity_id'): validate_cancel_timer_entity
     })
 
 
-    def call_switch_service(timer_entity, action):
+    def handle_timer_entity(timer_entity, action):
         switch_entity = "switch."+timer_entity[13:]
+        _LOGGER.info(f"Timer duration over on {timer_entity}")
+
         hass.services.call("switch", action, {"entity_id": switch_entity})
+        hass.states.set(timer_entity, "idle")
 
 
-    async def set_timer_handler(call):
+    def set_timer_handler(call):
         global unsub_dict
         data = dict(call.data)
 
@@ -113,19 +114,16 @@ def setup(hass, config):
             raise HomeAssistantError(f"Invalid service data: {error}")
 
 
-        entity_id = validated_data['entity_id']
-        component = hass.data[DOMAIN]
-        entity = component.get_entity(entity_id)
+        timer_entity = validated_data['entity_id']
         passed_time = datetime.strptime(validated_data['duration'], "%H:%M:%S").time()
         time_to_run = datetime.now(timezone.utc) + timedelta(hours=passed_time.hour, minutes=passed_time.minute, seconds=passed_time.second)
-        if entity:
-            await entity.async_update_state("set", {"finishing_at": time_to_run.strftime('%Y-%m-%dT%H:%M:%S.%f%z'), "duration": validated_data['duration'], "action": validated_data['action']})
-            unsub_dict[entity_id] = async_track_point_in_time(hass, lambda call: call_switch_service(entity_id, validated_data['action']), time_to_run)
-            _LOGGER.info(f"Timer set for {entity_id} entity. Duration: {validated_data['duration']}, Action: {validated_data['action']}")
+        hass.states.set(timer_entity, "set", {"finishing_at": time_to_run.strftime('%Y-%m-%dT%H:%M:%S.%f%z'), "duration": validated_data['duration'], "action": validated_data['action']})
+        unsub_dict[timer_entity] = async_track_point_in_time(hass, lambda call: handle_timer_entity(timer_entity, validated_data['action']), time_to_run)
+        _LOGGER.info(f"Timer set for {timer_entity} entity. Duration: {validated_data['duration']}, Action: {validated_data['action']}")
 
 
     
-    async def cancel_timer_handler(call):
+    def cancel_timer_handler(call):
         global unsub_dict
 
         data = dict(call.data)
@@ -135,14 +133,11 @@ def setup(hass, config):
             raise HomeAssistantError(error)
 
 
-        entity_id = validated_data['entity_id']
-        component = hass.data[DOMAIN]
-        entity = component.get_entity(entity_id)
-        if entity:
-            await entity.async_update_state("idle", clear_attributes=True)
-            unsub_dict[entity_id]()
-            unsub_dict.pop(entity_id)
-            _LOGGER.info(f"Timer canceled for {entity_id}")
+        timer_entity = validated_data['entity_id']
+        hass.states.set(timer_entity, "idle")
+        unsub_dict[timer_entity]()
+        unsub_dict.pop(timer_entity)
+        _LOGGER.info(f"Timer canceled for {timer_entity}")
         
     
     hass.services.register(DOMAIN, 'set_timer', set_timer_handler)
